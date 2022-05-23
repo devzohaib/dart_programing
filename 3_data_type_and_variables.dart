21f10536559cad15b0a93c08638f80c9c2e25ff6
zohaib <dev.zohaib7@gmail.com>
2022-05-23 08:01:21 +0500

    folders

---
+++ 3_data_type_and_variables.dart
@@ -0,0 +1,11 @@
+
+/* Variables are used for storing information which can then be used by the computer program
+
+Syntax for declaring a variable in Dart
+dataType variableName = initial_value;
+
+*/
+void main(){
+    var a;
+    print(a);
+}
